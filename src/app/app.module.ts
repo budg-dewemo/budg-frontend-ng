858c95cad840cd736bd0e1804f4d6b59a7f815e8
antoniamescia <antonia.mescia.novarese@gmail.com>
2022-09-28 20:11:26 -0300

    sign in view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignInButtonComponent } from './components/sign-in-button/sign-in-button.component';
+
+import { MatButtonModule }  from '@angular/material/button';
+import { SignInViewComponent } from './components/sign-in-view/sign-in-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInButtonComponent,
+    SignInViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
