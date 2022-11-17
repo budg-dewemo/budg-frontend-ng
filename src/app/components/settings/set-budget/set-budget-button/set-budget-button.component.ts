2b44bad301719e9d3d49310650b13a3b517b705e
antoniamescia <antonia.mescia.novarese@gmail.com>
2022-11-17 16:33:03 -0300

    commiteando porque tengo miedo

---
+++ src/app/components/settings/set-budget/set-budget-button/set-budget-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-set-budget-button',
+  templateUrl: './set-budget-button.component.html',
+  styleUrls: ['./set-budget-button.component.css']
+})
+export class SetBudgetButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
